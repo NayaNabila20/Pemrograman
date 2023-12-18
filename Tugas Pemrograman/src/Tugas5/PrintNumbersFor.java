4f61bd8564eea3502cdb5b6587bb781f625379ff
NayaNabila20 <151020892+NayaNabila20@users.noreply.github.com>
2023-12-18 20:03:25 +0700

    Delete Tugas Pemrograman directory

--- Tugas Pemrograman/src/Tugas5/PrintNumbersFor.java
+++
@@ -1,9 +0,0 @@
-package Tugas5;
-
-public class PrintNumbersFor{
-    public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(i);
-        }
-    }
-}
